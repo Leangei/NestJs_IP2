7a7a75094735924ab7e5e21ff05ef4887774a396
leangei <leangeimeng1602@gmail.com>
2026-06-11 08:43:19 +0700

    TP06

--- TP05-Hasura/src/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import TodoView from './views/TodoView.vue'
-</script>
-
-<template>
-  <TodoView />
-</template>
--- TP05-Hasura/src/components/HelloWorld.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <div>HelloWorld</div>
-</template>
-
